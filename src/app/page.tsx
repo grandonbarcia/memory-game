6d2306cd5144d3b69cd6be99d3ed1672bcaa629c
Brandon Garcia <grandonleebarcia@gmail.com>
2023-12-14 15:21:57 -0800

    start logic on gmae

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,43 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const nums = [
-  [0, 0, 0, 0],
-  [0, 0, 0, 0],
-  [0, 0, 0, 0],
-  [0, 0, 0, 0],
+  [1, 4, 4, 9],
+  [7, 6, 3, 8],
+  [6, 3, 5, 1],
+  [7, 9, 5, 8],
+];
+
+const display = [
+  [false, false, false, false],
+  [false, false, false, false],
+  [false, false, false, false],
+  [false, false, false, false],
 ];
 
 export default function Home() {
   const [data, setData] = useState([...nums]);
+  const [show, setShow] = useState([...display]);
+  const [clicked, setClicked] = useState(0);
+
+  function handleClick(colIdx: number, rowIdx: number) {
+    if (clicked < 2) {
+      let cloneArr = [...show];
+      cloneArr[colIdx][rowIdx] = true;
+      setShow([...cloneArr]);
+      setClicked(clicked + 1);
+    }
+  }
+
+  useEffect(() => {
+    if (clicked === 2) {
+    }
+  }, [clicked]);
 
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center p-24">
+      <main className="flex min-h-screen flex-col items-center p-24 gap-10">
         <div className="flex w-3/6 justify-between">
           <div>memory game</div>
           <div className="flex w-1/6 justify-between">
@@ -22,14 +45,31 @@ export default function Home() {
             <div>New Game</div>
           </div>
         </div>
-        <div className="flex flex-col gap-10">
+        <div className="flex w-3/6 justify-center items-center flex-col gap-5 ">
           {data.map((row, rowIdx) => (
-            <div className="flex gap-10">
+            <div
+              key={rowIdx}
+              className="flex gap-3 w-full text-center justify-center items-center"
+            >
               {row.map((col, colIdx) => (
-                <div>{col}</div>
+                <div
+                  key={colIdx}
+                  className="flex justify-center items-center w-32 h-32"
+                  onClick={() => handleClick(rowIdx, colIdx)}
+                >
+                  {show[rowIdx][colIdx] ? (
+                    col
+                  ) : (
+                    <div className="h-full w-full bg-red-400 rounded-full"></div>
+                  )}
+                </div>
               ))}
             </div>
           ))}
+        </div>
+        <div className="flex gap-5">
+          <div>Time 0:00</div>
+          <div>Moves 0 </div>
         </div>
       </main>
     </>
