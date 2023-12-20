fc355151ed8b1b7fbe7ca7704f25a9503e2968e4
Brandon Garcia <grandonleebarcia@gmail.com>
2023-12-19 21:24:40 -0800

    added top level state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,15 @@ import { useState, useEffect } from 'react';
 import Game from './components/Game';
 import StartMenu from './components/StartMenu';
 export default function Home() {
+  const [rules, setRules] = useState({});
+
+  useEffect(() => {
+    console.log(rules);
+  }, [rules]);
   return (
     <>
       <main className="">
-        <StartMenu />
+        <StartMenu setRules={setRules} />
       </main>
     </>
   );
